49d13761fa59bcf172c0b8f45e157e31ccb921df
yangshuai <yangshuai@xroom.net>
2021-11-09 09:39:45 +0800

    优化功能

--- XJHeartLighting/XJHeartViewController.swift
+++ XJHeartLighting/XJHeartViewController.swift
@@ -11,67 +11,71 @@ import UIKit
 var loopIndex: Int = 0
 /// 循环时间
 var timeInterval: TimeInterval = 0.2
+/// 切换时间
+var switchTimeInterval: TimeInterval = 1.0
 // 熄灭颜色
 let extinctColor = UIColor.black
 // 点亮颜色
 let lightingColor = UIColor.red
 
 // MARK: - 点亮模式
-enum XJHeartMode {
+enum XJHeartMode: Int {
     
     // 点亮一圈
-    case one_clockwise_all    // 顺时针一圈连续
-    case one_anticlockwise_all // 逆时针一圈连续
-    case one_clockwise_one      // 顺时针一圈一个接一个
-    case one_anticlockwise_one  // 逆时针一圈一个接一个
+    case one_clockwise_all              // 顺时针一圈连续
+    case one_anticlockwise_all          // 顺时针一圈连续
+    case one_clockwise_one              // 顺时针一圈一个接一个
+    case one_anticlockwise_one          // 逆时针一圈一个接一个
     
     // 点亮半圈
-    case half_clockwise_all
-    case half_anticlockwise_all
-    case half_clockwise_one
-    case half_anticlockwise_one
+    case half_clockwise_all             // 从上至下半圈连续
+    case half_anticlockwise_all         // 从下至上圈连续
+    case half_clockwise_one             // 从上至下半圈一个接一个
+    case half_anticlockwise_one         // 从下至上半圈一个接一个
     
     // 点亮1234象限
-    case quarter1234_clockwise_all
-    case quarter1234_anticlockwise_all
-    case quarter1234_clockwise_aone
-    case quarter1234_anticlockwise_one
+    case quarter1234_clockwise_all      // 顺时针全象限连续
+    case quarter1234_anticlockwise_all  // 顺时针全象限连续
+    case quarter1234_clockwise_one      // 顺时针全象限一个接一个
+    case quarter1234_anticlockwise_one  // 逆时针全象限一个接一个
     
     // 点亮13象限
-    case quarter13_clockwise_all
-    case quarter13_anticlockwise_all
-    case quarter13_clockwise_aone
-    case quarter13_anticlockwise_one
+    case quarter13_clockwise_all        // 顺时针13象限连续
+    case quarter13_anticlockwise_all    // 顺时针13象限连续
+    case quarter13_clockwise_one        // 顺时针13象限一个接一个
+    case quarter13_anticlockwise_one    // 逆时针13象限一个接一个
     
     // 点亮24象限
-    case quarter24_clockwise_all
-    case quarter24_anticlockwise_all
-    case quarter24_clockwise_aone
-    case quarter24_anticlockwise_one
+    case quarter24_clockwise_all        // 顺时针24象限连续
+    case quarter24_anticlockwise_all    // 顺时针24象限连续
+    case quarter24_clockwise_one        // 顺时针24象限一个接一个
+    case quarter24_anticlockwise_one    // 逆时针24象限一个接一个
     
     // 点亮14象限
-    case quarter14_clockwise_all
-    case quarter14_anticlockwise_all
-    case quarter14_clockwise_aone
-    case quarter14_anticlockwise_one
+    case quarter14_clockwise_all        // 顺时针14象限连续
+    case quarter14_anticlockwise_all    // 顺时针14象限连续
+    case quarter14_clockwise_one        // 顺时针14象限一个接一个
+    case quarter14_anticlockwise_one    // 逆时针14象限一个接一个
     
     // 点亮23象限
-    case quarter23_clockwise_all
-    case quarter23_anticlockwise_all
-    case quarter23_clockwise_aone
-    case quarter23_anticlockwise_one
+    case quarter23_clockwise_all        // 顺时针23象限连续
+    case quarter23_anticlockwise_all    // 顺时针23象限连续
+    case quarter23_clockwise_one        // 顺时针23象限一个接一个
+    case quarter23_anticlockwise_one    // 逆时针23象限一个接一个
     
-    case allin_allout
-    case left_right
-    case top_bottom
-    case leftup_rightbottom
-    case rightup_leftbottom
-    case odd_even
+    // 亮熄切换
+    case allin_allout                   // 全亮全熄切换
+    case left_right                     // 左右切换
+    case top_bottom                     // 上下切换
+    case leftup_rightbottom             // 左上右下切换
+    case rightup_leftbottom             // 右上左下切换
+    case odd_even                       // 奇偶切换
 }
 
 // MARK: - 控制器
 class XJHeartViewController: UIViewController {
     
+    /// UI图
     lazy var heartView: XJHeartView = {
         let heart = XJHeartView(frame: self.view.bounds)
         return heart
@@ -79,7 +83,6 @@ class XJHeartViewController: UIViewController {
     
     /// 定时器
     var timer: Timer?
-    var switchTimer: Timer?
     
     /// 点亮模式
     var heartMode: XJHeartMode = .one_clockwise_all
@@ -95,11 +98,10 @@ class XJHeartViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         loopIndex = 0
         self.title = "流水灯"
-        
         self.view.addSubview(heartView)
-        
         self.createTimer(timeInterval, isFire: false)
     }
     
@@ -107,34 +109,17 @@ class XJHeartViewController: UIViewController {
     func createTimer(_ timeInterval: TimeInterval, isFire: Bool) {
         timer = Timer.scheduledTimer(withTimeInterval: timeInterval, repeats: true) {[weak self] (timer) in
             guard let self = self else { return }
-            
             self.executionTimer()
         }
         if !isFire {
             timer?.fireDate = .distantFuture
         }
-        
     }
     
     /// 销毁定时器
     func invalidateTimer() {
         guard let timer = timer else { return }
         timer.invalidate()
-    }
-    
-    /// 创建定时器--用于切换
-    func createSwitchTimer(_ timeInterval: TimeInterval) {
-        switchTimer = Timer.scheduledTimer(withTimeInterval: timeInterval, repeats: true) {[weak self] (timer) in
-            guard let self = self else { return }
-            
-            self.executionTimer()
-        }
-    }
-    
-    /// 销毁定时器--用于切换
-    func invalidateSwitchTimer() {
-        guard let switchTimer = switchTimer else { return }
-        switchTimer.invalidate()
     }
     
     /// 触发定时器
@@ -145,7 +130,6 @@ class XJHeartViewController: UIViewController {
     /// 点击返回
     func backBtnClick(_ button: UIButton) {
         self.invalidateTimer()
-        self.invalidateSwitchTimer()
     }
     
     /// 设置模式
@@ -157,27 +141,18 @@ class XJHeartViewController: UIViewController {
         // 设置新模式
         self.heartMode = heartMode
         
-        // 销毁切换定时器
-        self.invalidateSwitchTimer()
-        
         // 销毁旧定时器
         self.invalidateTimer()
         
         // 创新新定时器
-        if heartMode == .allin_allout || heartMode == .left_right || heartMode == .top_bottom || heartMode == .leftup_rightbottom || heartMode == .rightup_leftbottom || heartMode == .odd_even {
-            
-            self.createSwitchTimer(1.0)
-        } else {
-            self.createTimer(timeInterval, isFire: true)
-        }
+        let time = heartMode.rawValue >= 28 ? switchTimeInterval : timeInterval
+        self.createTimer(time, isFire: true)
     }
     
     /// 执行定时器
     func executionTimer() {
-        
         self.personalizedDesignLighting()
     }
-
 }
 
 // MARK: - 个性化设计点亮方式
